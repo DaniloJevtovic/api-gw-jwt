52eba21e9f5ca7b7f73931efe04cdbf6d7b59127
DaniloJevtovic <danilo_jevtovic@hotmail.com>
2023-04-27 21:58:00 +0200

    dodati kontroleri po ulogama

---
+++ service2/src/main/java/com/lemur/service2/AdminController.java
@@ -0,0 +1,16 @@
+package com.lemur.service2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/service2/admin")
+public class AdminController {
+
+    // ADMIN
+    @GetMapping
+    public String adminService() {
+        return "Admin service";
+    }
+}
---
+++ service2/src/main/java/com/lemur/service2/ModeratorController.java
@@ -0,0 +1,16 @@
+package com.lemur.service2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/service2/moderator")
+public class ModeratorController {
+
+    // MODERATOR
+    @GetMapping
+    public String moderatorService() {
+        return "Moderator service";
+    }
+}
